39561cf55456e77987394e33f14fb5a14724c44e
jason8105 <jasondsilva8197@gmail.com>
2026-08-02 08:48:43 +0000

    fix: resolve build and compilation errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,24 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+id("com.android.application")
+id("org.jetbrains.kotlin.android")
 }
-
 android {
-    namespace = "com.zygisk.imgui.touchfix"
-    compileSdk = 34
+namespace = "com.reveny.modmenu"
+compileSdk = 33
+Copy code
+defaultConfig {
+    applicationId = "com.reveny.modmenu"
+    minSdk = 29
+    targetSdk = 33
+    versionCode = 1
+    versionName = "1.0"
 
-    defaultConfig {
-        applicationId = "com.zygisk.imgui.touchfix"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    externalNativeBuild {
-        cmake {
-            path = file("CMakeLists.txt")
-            version = "3.22.1"
-        }
-    }
-
-    ndkVersion = "25.2.9519653"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-}
+buildTypes {
+    release {
+        isMinifyEnabled = false
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules
