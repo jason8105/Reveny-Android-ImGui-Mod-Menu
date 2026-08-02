d1f68ea8865e514681de5ed38baab71cd0746d58
jason8105 <jasondsilva8197@gmail.com>
2026-08-02 09:14:21 +0000

    fix: resolve build and compilation errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,9 @@ compileSdk = 33
 Copy code
 defaultConfig {
     applicationId = "com.reveny.modmenu"
+    minSdk = 29
+    targetSdk = 33
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx
