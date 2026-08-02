fc2b26dcd17994590dfc97fa62d4319c255adb11
jason8105 <jasondsilva8197@gmail.com>
2026-08-02 08:49:48 +0000

    fix: resolve build and compilation errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,14 +11,4 @@ defaultConfig {
     minSdk = 29
     targetSdk = 33
     versionCode = 1
-    versionName = "1.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-}
-
-buildTypes {
-    release {
-        isMinifyEnabled = false
-        proguardFiles(
-            getDefaultProguardFile("proguard-android-optimize.txt"),
-            "proguard-rules
+    versionName = "1
