0c40276d2015c9dc56cf58d3f946d3ef404aa01f
jason8105 <jasondsilva8197@gmail.com>
2026-08-02 09:09:56 +0000

    fix: resolve build and compilation errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,4 @@ namespace = "com.reveny.modmenu"
 compileSdk = 33
 Copy code
 defaultConfig {
-    applicationId = "com.reveny.modmenu"
-    minSdk = 29
-    targetSdk = 33
-    versionCode = 1
-    versionName = "1
+    applicationId = "com.reveny
